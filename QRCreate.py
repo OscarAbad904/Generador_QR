56580ae31f6737eee77fa4bfa05d36d87ef644d9
OscarAbad904 <sinnombre904@hotmail.com>
2023-12-10 15:25:09 +0100

    Create QRCreate.py

---
+++ QRCreate.py
@@ -0,0 +1,21 @@
+import pyqrcode
+import sys
+
+def QRCreate(TextoQR, RutaQR, Size):
+    qr = pyqrcode.create(TextoQR)
+    qr.png(RutaQR, scale = Size)
+
+def main():
+    if len(sys.argv) != 4:
+        print("Uso: QRCreate <TextoQR> <RutaQR> <Size>")
+        sys.exit(1)
+
+    TextoQR = sys.argv[1]
+    RutaQR = sys.argv[2]
+    Size = sys.argv[3]
+
+    QRCreate(TextoQR, RutaQR, Size)
+    print(f"¡¡¡Código QR creado exitosamente!!!\nEn el archivo: {RutaQR}")
+
+if __name__ == "__main__":
+    main() 
